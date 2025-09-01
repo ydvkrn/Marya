f918d7d43fcb531ad753ea76291441658228fc1e
ydvkrn <kyadav00011199@gmail.com>
2025-09-01 11:49:54 +0530

    Create _functions/_config.js

---
+++ _functions/_config.js
@@ -0,0 +1,6 @@
+export const BOT_TOKEN   = '8360624116:AAEE…mbuaE';     // <-- अपनी
+export const CHANNEL_ID  = '-1003071466750';            // channel @ID
+export const MAX_SIZE    = 2 * 1024 * 1024 * 1024;      // 2 GB
+export const CHUNK       = 20 * 1024 * 1024;            // 20 MB
+export const CACHE_SECS  = 315360000;                   // 10 yr
+export const PASSCODE    = 'MSM@MARYA';                  // admin panel
