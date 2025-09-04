d4b0d8ff206704d5c04547f7c11527c5bc2fd570
ydvkrn <kyadav00011199@gmail.com>
2025-09-04 10:33:12 +0530

    Delete functions directory

--- functions/_config.js
+++
@@ -1,5 +0,0 @@
-export const BOT_TOKEN = '8360624116:AAEEJha8CRgL8TnrEKk5zOuCNXXRawmbuaE';
-export const CHANNEL_ID = '-1003071466750';
-export const MAX_SIZE = 2 * 1024 * 1024 * 1024; // 2GB
-export const CACHE_SECS = 315360000; // 10 years
-export const PASSCODE = 'MSM@MARYA';
