f4ca7da6b49558c93097adbf224f6c77b95060d1
ydvkrn <kyadav00011199@gmail.com>
2025-09-04 08:52:25 +0530

    Update _config.js

--- functions/_config.js
+++ functions/_config.js
@@ -1,6 +1,4 @@
-export const BOT_TOKEN   = '8360624116:AAEE…mbuaE';     // <-- अपनी
-export const CHANNEL_ID  = '-1003071466750';            // channel @ID
-export const MAX_SIZE    = 2 * 1024 * 1024 * 1024;      // 2 GB
-export const CHUNK       = 20 * 1024 * 1024;            // 20 MB
-export const CACHE_SECS  = 315360000;                   // 10 yr
-export const PASSCODE    = 'MSM@MARYA';                  // admin panel
+export const BOT_TOKEN = '8360624116:AAEEJha8CRgL8TnrEKk5zOuCNXXRawmbuaE';
+export const CHANNEL_ID = '-1003071466750'; 
+export const MAX_SIZE = 2 * 1024 * 1024 * 1024; // 2GB
+export const CACHE_SECS = 315360000; // 10 years
